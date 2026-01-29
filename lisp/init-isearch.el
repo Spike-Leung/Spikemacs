d4a742756f550ffcdf7dc22963fd69b2be4248dc
Spike <l-yanlei@hotmail.com>
2026-01-29 11:29:57 +0800

    :sparkles: add spike-leung/consult-line-symbol-at-point

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -19,7 +19,9 @@
          ("C-o" . sanityinc/isearch-occur)
          ("C-c C-o" . sanityinc/isearch-occur)
          ("C-M-w" . isearch-yank-symbol)
-         ("C-<return>" . sanityinc/isearch-exit-other-end))
+         ("C-<return>" . sanityinc/isearch-exit-other-end)
+         ("M-s ." . spike-leung/consult-line-symbol-at-point)
+         ("M-s M-s ." . isearch-forward-thing-at-point))
   :config
   (defun sanityinc/isearch-occur ()
     "Invoke `consult-line' from isearch."
@@ -50,7 +52,14 @@
 This is useful when followed by an immediate kill."
     (interactive)
     (isearch-exit)
-    (goto-char isearch-other-end)))
+    (goto-char isearch-other-end))
+
+  ;; see: https://arialdomartini.github.io/consult-line-at-point
+  (defun spike-leung/consult-line-symbol-at-point ()
+    "Search for a line matching the symbol found near point."
+    (interactive)
+    (consult-line
+     (or (thing-at-point 'symbol)))))
 
 
 
