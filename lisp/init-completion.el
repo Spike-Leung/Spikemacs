7282bb3ea5605ddbe6ffa452622e4a43164e173e
Spike <l-yanlei@hotmail.com>
2026-02-03 02:06:24 +0800

    :sparkles: try cape.el

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -41,6 +41,24 @@
   :config (corfu-terminal-mode))
 
 
+;;; cape.el - Completion At Point Extensions
+;; https://github.com/minad/cape
+
+(use-package cape
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-keyword)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  (add-hook 'completion-at-point-functions #'cape-history)
+  (add-hook 'completion-at-point-functions #'cape-line))
+
+
 
 (use-package kind-icon
   :ensure t
