4fd4e3664ef9206b1916b654b7845b54cba2f09f
Spike <l-yanlei@hotmail.com>
2026-02-03 01:49:03 +0800

    :sparkles: switch to dirvish and refine configuration
    - Ignore `dirvish/` cache directory in `.gitignore` Replace
    - `nerd-icons-dired` and `dired-preview` with `dirvish` Enable
    - `dirvish-override-dired-mode` and configure: Custom quick access
    - entries Mode line and attributes (icons, vc-state, etc.)
    - Keybindings for navigation, jumping, and menu dispatching Update
    - `dired-listing-switches` for better default display

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -15,7 +15,14 @@
 	      ("C-c C-q" . wdired-change-to-wdired-mode))
   :config
   (setq-default dired-dwim-target t)
-  (setq dired-recursive-deletes 'top))
+  (setq dired-recursive-deletes 'top)
+
+  ;; this command is useful when you want to close the window of `dirvish-side'
+  ;; automatically when opening a file
+  ;; (put 'dired-find-alternate-file 'disabled nil)
+
+  (setq dired-listing-switches
+        "-l --almost-all --human-readable --group-directories-first --no-group"))
 
 
 
@@ -34,10 +41,11 @@
 
 
 ;; icons
-(use-package nerd-icons-dired
-  :diminish
-  :requires nerd-icons
-  :hook (dired-mode . nerd-icons-dired-mode))
+;; dirvish 已经有 icon 了，再使用 nerd-icons-dired 会出现重复 icon
+;; (use-package nerd-icons-dired
+;;   :diminish
+;;   :requires nerd-icons
+;;   :hook (dired-mode . nerd-icons-dired-mode))
 
 
 
@@ -47,7 +55,52 @@
 
 
 
-(use-package dired-preview)
+;; dirvish 实现了类似功能了
+;; (use-package dired-preview)
+
+
+;;; dirvish
+;; for preview on MacOS, you need to install some deps:
+;; run "brew install fd poppler ffmpegthumbnailer mediainfo vips 7zip imagemagick"
+;; see also: https://github.com/alexluigit/dirvish/blob/main/docs/CUSTOMIZING.org#install-dependencies-for-an-enhanced-preview-experience
+(use-package dirvish
+  :init (dirvish-override-dired-mode)
+  :custom (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+           '(("h" "~/"                          "Home")
+             ("d" "~/Downloads/"                "Downloads")
+             ("a" "~/git/taxodium/publish/images/album"                "Blog Album")
+             ("e" "~/.emacs.d/" "Emacs Config")))
+  :config
+  ;; (dirvish-peek-mode)             ; Preview files in minibuffer
+  ;; (dirvish-side-follow-mode)      ; similar to `treemacs-follow-mode'
+  (setq dirvish-mode-line-format
+        '(:left (sort symlink) :right (omit yank index)))
+  (setq dirvish-attributes           ; The order *MATTERS* for some attributes
+        '(vc-state subtree-state nerd-icons collapse file-time file-size)
+        dirvish-side-attributes
+        '(vc-state nerd-icons collapse file-size))
+  ;; open large directory (over 20000 files) asynchronously with `fd' command
+  (setq dirvish-large-directory-threshold 20000)
+  :bind ; Bind `dirvish-fd|dirvish-side|dirvish-dwim' as you see fit
+  (("C-c f" . dirvish)
+   :map dirvish-mode-map               ; Dirvish inherits `dired-mode-map'
+   (";"   . dired-up-directory)        ; So you can adjust `dired' bindings here
+   ("?"   . dirvish-dispatch)          ; [?] a helpful cheatsheet
+   ("a"   . dirvish-setup-menu)        ; [a]ttributes settings:`t' toggles mtime, `f' toggles fullframe, etc.
+   ("f"   . dirvish-file-info-menu)    ; [f]ile info
+   ("o"   . dirvish-quick-access)      ; [o]pen `dirvish-quick-access-entries'
+   ("s"   . dirvish-quicksort)         ; [s]ort flie list
+   ("r"   . dirvish-history-jump)      ; [r]ecent visited
+   ("l"   . dirvish-ls-switches-menu)  ; [l]s command flags
+   ("v"   . dirvish-vc-menu)           ; [v]ersion control commands
+   ("*"   . dirvish-mark-menu)
+   ("y"   . dirvish-yank-menu)
+   ("N"   . dirvish-narrow)
+   ("^"   . dirvish-history-last)
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-e" . dirvish-emerge-menu)))
 
 
 
