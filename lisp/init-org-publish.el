fe001161c8f84ecb4e2a1ab7b885675ca0c5b6fc
Spike <l-yanlei@hotmail.com>
2026-08-16 19:30:22 +0800

    org-publish: ox-ascii: set `org-ascii-links-to-notes` to `t`

--- lisp/init-org-publish.el
+++ lisp/init-org-publish.el
@@ -366,7 +366,7 @@ holding contextual information."
   (org-ascii-text-width 88)
   (org-ascii-quote-margin 2)
   (org-ascii-charset 'ascii)
-  (org-ascii-links-to-notes nil)
+  (org-ascii-links-to-notes t)
   :config
   ;; do not interpret *word*, /word/, _word_ and +word+
   (defun org-ascii-bold (_bold contents _info) contents)
